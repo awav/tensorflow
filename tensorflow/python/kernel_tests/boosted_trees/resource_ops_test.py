5bcf84e9f1dde828f9215aad3515765755f7cf89
Mihai Maruseac <mihaimaruseac@google.com>
2020-12-23 19:43:19 -0800

    Split and add

--- tensorflow/python/kernel_tests/boosted_trees/resource_ops_test.py
+++ tensorflow/python/kernel_tests/boosted_trees/resource_ops_test.py
@@ -153,6 +153,13 @@ class ResourceOpsTest(test_util.TensorFlowTestCase):
           'ensemble',
           stamp_token=7,
           serialized_proto=ensemble_proto.SerializeToString())
+      _p("testCreateWithProto 5", file=sys.stderr)
+      r = resources.shared_resources()
+      _p("testCreateWithProto 6", file=sys.stderr)
+      r=resources.initialize_resources(r)
+      _p("testCreateWithProto 7", file=sys.stderr)
+      r.run()
+      _p("testCreateWithProto 8", file=sys.stderr)
 
 
 if __name__ == '__main__':
