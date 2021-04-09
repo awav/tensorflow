2f42c21d38f76aefc97f9b54e8188c056972a3b0
Tilman Roeder <dyed.green.info@gmail.com>
2021-04-10 00:20:40 +0100

    first test with hlo

--- tensorflow/compiler/xla/service/do_random_shit.cc
+++ tensorflow/compiler/xla/service/do_random_shit.cc
@@ -6,7 +6,21 @@
 namespace xla {
 
 StatusOr<bool> RandomShitPass::Run(HloModule* module) {
-  printf("This is a test!\n");
+  if (!module->has_entry_computation()) {
+    printf("DELETE ME: no entry computatio\n");
+    return false;
+  }
+
+  HloComputation* entry = module->entry_computation();
+  HloInstruction* root = entry->root_instruction();
+
+  if (!root) {
+    printf("DELETE ME: No root instruction\n");
+    return false;
+  }
+
+  printf("Root OptCode: %s\n", HloOpcodeString(root->opcode()).c_str());
+
   return false;
 }
 
