48f60146531e2366d94d3ba2a2471a7a8ccd1036
Tilman Roeder <dyed.green.info@gmail.com>
2021-04-12 13:25:01 +0100

    Get test to work

--- tensorflow/compiler/xla/service/rewriting_optimizer_test.cc
+++ tensorflow/compiler/xla/service/rewriting_optimizer_test.cc
@@ -17,8 +17,33 @@ class RewritingOptimizerTest : public HloTestBase {};
 
 // Test (AB)C => A(BC)
 TEST_F(RewritingOptimizerTest, MatrixVectorDot) {
-  EXPECT_EQ(true, false);
+  auto m = CreateNewVerifiedModule();
+  HloComputation::Builder builder(TestName());
+
+  Shape mat_shape = ShapeUtil::MakeShape(F32, {1000, 2});
+  HloInstruction* a = builder.AddInstruction(HloInstruction::CreateParameter(0, mat_shape, "a"));
+  HloInstruction* b = builder.AddInstruction(HloInstruction::CreateParameter(1, mat_shape, "b"));
+  HloInstruction* c = builder.AddInstruction(HloInstruction::CreateParameter(2, mat_shape, "c"));
+
+  DotDimensionNumbers dnums_ab;
+  dnums_ab.add_lhs_contracting_dimensions(1);
+  dnums_ab.add_rhs_contracting_dimensions(1);
+  Shape ab_shape = ShapeUtil::MakeShape(F32, {1000, 1000});
+  HloInstruction* ab = builder.AddInstruction(HloInstruction::CreateDot(ab_shape, a, b, dnums_ab, DefaultPrecisionConfig(2)));
+
+  DotDimensionNumbers dnums_abc;
+  dnums_abc.add_lhs_contracting_dimensions(1);
+  dnums_abc.add_rhs_contracting_dimensions(0);
+  Shape abc_shape = ShapeUtil::MakeShape(F32, {1000, 2}); 
+  builder.AddInstruction(HloInstruction::CreateDot(abc_shape, ab, c, dnums_abc, DefaultPrecisionConfig(2)));
+
+  m->AddEntryComputation(builder.Build());
+  RewritingOptimizer optim;
+  TF_ASSERT_OK_AND_ASSIGN(bool result, RunHloPass(&optim, m.get()));
+  EXPECT_TRUE(result);
+
+  // TODO: Check result has correct shape
 }
 
-}
+} // namespace
 } // namespace xla
