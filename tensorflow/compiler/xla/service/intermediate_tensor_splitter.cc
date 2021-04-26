c92b0b16dadbadb36270b3850c02b7f18b3a7384
Tilman Roeder <dyed.green.info@gmail.com>
2021-04-26 15:32:16 +0100

    Compile first version

--- tensorflow/compiler/xla/service/intermediate_tensor_splitter.cc
+++ tensorflow/compiler/xla/service/intermediate_tensor_splitter.cc
@@ -111,7 +111,12 @@ IntermediateTensorSplitterVisitor::BuildComputationAndParameters(
     // Generate the final dot operation, as well as the operand
     // vector.
     auto& dnums = inst->dot_dimension_numbers();
-    int64 dims_lhs = lhs->shape().rank() - dnums->lhs_contracting_dimensions_size();
+    int64 dims_lhs =
+        lhs->shape().rank() - dnums.lhs_contracting_dimensions_size();
+
+    Shape dot_shape = ShapeUtil::MakeShape(inst->shape().element_type(),
+                                           inst->shape().dimensions());
+    dot_shape.set_dimensions(split_dim, split_size);
 
     HloInstruction *split_op, *join_op;
     bool split_is_lhs;
@@ -130,17 +135,59 @@ IntermediateTensorSplitterVisitor::BuildComputationAndParameters(
 
     // adjust split dim up for every contraction dimension to it's left
     // TODO: Check if this is robust for multiple indices ...
-    for (int64 i = 0; i < dnums->lhs_contracting_dimensions_size(); i ++) {
-      if (split_dim >= dnums->lhs_contracting_dimensions(i))
-        split_dim += 1;
+    for (int64 i = 0; i < dnums.lhs_contracting_dimensions_size(); i++) {
+      if (split_dim >= dnums.lhs_contracting_dimensions(i)) split_dim += 1;
     }
 
     // generate parameters for each split
     int64 dims_done = 0;
-    // each split should have an aeeay allocated already
-    // TODO
+    int64 split_parameter_idx, join_parameter_idx;
 
-    CHECK(false);
+    Shape split_shape = ShapeUtil::MakeShape(split_op->shape().element_type(),
+                                             split_op->shape().dimensions());
+    split_shape.set_dimensions(split_dim, split_size);
+
+    std::vector<int64> start, limit, stride;
+    for (int64 dim; dim < split_op->shape().dimensions_size(); dim++) {
+      start.push_back(0);
+      limit.push_back(split_op->shape().dimensions(dim));
+      stride.push_back(1);
+    }
+
+    for (int64 i = 0; dims_done < split_op->shape().dimensions(split_dim);
+         i++, dims_done += split_size) {
+      // build split parameter
+      split_parameter_idx = parameters->at(i).size();
+      start[split_dim] = dims_done;
+      limit[split_dim] = dims_done + split_size;
+      HloInstruction* slice = HloInstruction::CreateSlice(
+                                  split_shape, split_op, absl::MakeSpan(start),
+                                  absl::MakeSpan(limit), absl::MakeSpan(stride))
+                                  .release();
+      parameters->at(i).push_back(slice);
+      // attach join parameter
+      join_parameter_idx = parameters->at(i).size();
+      parameters->at(i).push_back(join_op);
+    }
+
+    // build the final dot
+    HloInstruction* split_param =
+        HloInstruction::CreateParameter(split_parameter_idx, split_shape,
+                                        "dot_split_tensor")
+            .release();
+    HloInstruction* join_param =
+        HloInstruction::CreateParameter(join_parameter_idx, join_op->shape(),
+                                        "dot_join_tensor")
+            .release();
+
+    std::vector<HloInstruction*> new_operands;
+    if (split_is_lhs) {
+      new_operands = {split_param, join_param};
+    } else {
+      new_operands = {join_param, split_param};
+    }
+    return builder->AddInstruction(
+        inst->CloneWithNewOperands(dot_shape, absl::MakeSpan(new_operands)));
   } else if (MatchPointwiseUnary(inst, &operand)) {
     // For a unary operation recursively obtain a new operand and
     // clone the operation.
@@ -180,15 +227,36 @@ Status IntermediateTensorSplitterVisitor::HandleDot(HloInstruction* dot) {
         BestSplitDim(lhs, absl::MakeSpan(dnums.lhs_contracting_dimensions()));
 
     HloComputation::Builder builder("intermediate_tensor_computation_builde");
-    std::vector<std::vector<HloInstruction*>> parameters; // TODO: allocate a vector for each split
+    std::vector<std::vector<HloInstruction*>> parameters;
+
+    int64 full_size = 0;
+    int64 split_size = 1000;
+    while (full_size < lhs->shape().dimensions(split_dim)) {
+      parameters.push_back({});
+      full_size += split_size;
+    }
+    CHECK(full_size = lhs->shape().dimensions(
+              split_dim));  // TODO: Handle potential odd last split size
+
     // TODO: Make the split size configurable (and smarter ...)
     TF_ASSIGN_OR_RETURN(HloInstruction * comp_root,
-                        BuildComputationAndParameters(lhs, split_dim, 1000,
-                                                      &builder, &parameters));
+                        BuildComputationAndParameters(
+                            lhs, split_dim, split_size, &builder, &parameters));
+    HloComputation* comp = builder.Build(comp_root).release();
 
-    // TODO !
+    // Create vector of calls
+    std::vector<HloInstruction*> calls;
+    for (auto operands : parameters) {
+      HloInstruction* call = HloInstruction::CreateCall(comp_root->shape(), absl::MakeSpan(operands), comp).release();
+      calls.push_back(call);
+    }
+
+    // create concat operation
+    HloInstruction* concat = HloInstruction::CreateConcatenate(dot->shape(), absl::MakeSpan(calls), split_dim).release();
+    return ReplaceInstruction(dot, concat);
   } else if (OperandShouldBeSplit(rhs) && OperandCanBeSplit(rhs)) {
     LOG(INFO) << "Will attempt to split rhs: TODO";
+    CHECK(false);
   }
 }
 
