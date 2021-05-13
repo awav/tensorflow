d6625902debd366543dc7af966b051a38ebe90ba
Tilman Roeder <dyed.green.info@gmail.com>
2021-05-13 13:40:51 +0100

    Add failing test for rhs case

--- tensorflow/compiler/xla/service/dot_order_optimizer_test.cc
+++ tensorflow/compiler/xla/service/dot_order_optimizer_test.cc
@@ -57,5 +57,47 @@ TEST_F(DotOrderOptimizerTest, MatrixVectorDotLhs) {
             m::Dot(m::Op().Is(a), m::Dot(m::Op().Is(b), m::Op().Is(c)))));
 }
 
+// TEST A(BC) => (AB)C
+TEST_F(DotOrderOptimizerTest, MatrixVectorDotRhs) {
+  auto m = CreateNewVerifiedModule();
+  HloComputation::Builder builder(TestName());
+
+  Shape mat_shape = ShapeUtil::MakeShape(F32, {1000, 2});
+  HloInstruction* a = builder.AddInstruction(
+      HloInstruction::CreateParameter(0, mat_shape, "a"));
+  HloInstruction* b = builder.AddInstruction(
+      HloInstruction::CreateParameter(1, mat_shape, "b"));
+  HloInstruction* c = builder.AddInstruction(
+      HloInstruction::CreateParameter(2, mat_shape, "c"));
+
+  DotDimensionNumbers dnums_bc;
+  dnums_bc.add_lhs_contracting_dimensions(1);
+  dnums_bc.add_rhs_contracting_dimensions(1);
+  Shape bc_shape = ShapeUtil::MakeShape(F32, {1000, 1000});
+  HloInstruction* bc = builder.AddInstruction(HloInstruction::CreateDot(bc_shape, b, c, dnums_bc,
+                                                   DefaultPrecisionConfig(2)));
+
+  DotDimensionNumbers dnums_abc;
+  dnums_abc.add_lhs_contracting_dimensions(0);
+  dnums_abc.add_rhs_contracting_dimensions(0);
+  Shape abc_shape = ShapeUtil::MakeShape(F32, {2, 1000});
+  builder.AddInstruction(HloInstruction::CreateDot(abc_shape, a, bc, dnums_abc,
+                                                   DefaultPrecisionConfig(2)));
+
+  HloComputation* computation = m->AddEntryComputation(builder.Build());
+
+  EXPECT_TRUE(
+      Match(computation->root_instruction(),
+            m::Dot(m::Op().Is(a), m::Dot(m::Op().Is(b), m::Op().Is(c)))));
+
+  DotOrderOptimizer optim;
+  TF_ASSERT_OK_AND_ASSIGN(bool result, RunHloPass(&optim, m.get()));
+  EXPECT_TRUE(result);
+
+  EXPECT_TRUE(
+      Match(computation->root_instruction(),
+            m::Dot(m::Dot(m::Op().Is(a), m::Op().Is(b)), m::Op().Is(c))));
+}
+
 }  // namespace
 }  // namespace xla
