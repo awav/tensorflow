737b9103fd5f2a56e9cd143172dc7631c30e9cf7
Artem Artemev <art.art.v@gmail.com>
2022-10-26 09:58:26 +0100

    Fix euclidean matrices

--- tensorflow/compiler/xla/service/algebraic_rewriter.cc
+++ tensorflow/compiler/xla/service/algebraic_rewriter.cc
@@ -35,7 +35,8 @@ bool AlgebraicRewriterVisitor::MatchDistanceMatrix(
   HloInstruction* core_expr = nullptr;
   HloInstruction* reduce_operand = nullptr;
   HloInstruction* reduce_init = nullptr;
-  if (!Match(reduce, m::Reduce(m::Op(&reduce_operand), m::Constant(&reduce_init)))) {
+  if (!Match(reduce,
+             m::Reduce(m::Op(&reduce_operand), m::Constant(&reduce_init)))) {
     return false;
   }
   auto is_pow_based = reduce_operand->opcode() == HloOpcode::kPower;
@@ -57,7 +58,7 @@ bool AlgebraicRewriterVisitor::MatchDistanceMatrix(
   }
 
   // Check add or sub
-  HloInstruction *lhs = nullptr;
+  HloInstruction* lhs = nullptr;
   HloInstruction* rhs = nullptr;
   if (Match(core_expr, m::Add(m::Op(&lhs), m::Op(&rhs)))) {
     *is_sub = false;
@@ -85,12 +86,18 @@ bool AlgebraicRewriterVisitor::MatchDistanceMatrix(
   *x_reduce_dim = -1;
   *y_reduce_dim = -1;
   for (int64_t i = 0; i < lhs->dimensions().size(); i++) {
-    if (lhs->dimensions(i) == reduce_dim) *x_reduce_dim = i;
+    if (lhs->dimensions(i) == reduce_dim) {
+      *x_reduce_dim = i;
+    }
   }
   for (int64_t i = 0; i < rhs->dimensions().size(); i++) {
-    if (rhs->dimensions(i) == reduce_dim) *y_reduce_dim = i;
+    if (rhs->dimensions(i) == reduce_dim) {
+      *y_reduce_dim = i;
+    }
   }
-  if (*x_reduce_dim == -1 || *y_reduce_dim == -1) return false;
+  if (*x_reduce_dim == -1 || *y_reduce_dim == -1) {
+    return false;
+  }
   // there must be a pair of dims i =/= j such that:
   // x -> i : R, j : B and y -> i : B, j : R
   CHECK(ShapeUtil::Equal(lhs->shape(), rhs->shape()));
@@ -100,8 +107,9 @@ bool AlgebraicRewriterVisitor::MatchDistanceMatrix(
   *y_dim = -1;
   for (int64_t i = 0; i < rank; i++) {
     for (int64_t j = 0; j < rank; j++) {
-      if (i == j) continue;
-      if (i == reduce_dim || j == reduce_dim) continue;
+      if (i == j || i == reduce_dim || j == reduce_dim) {
+        continue;
+      }
       if (absl::c_linear_search(lhs->dimensions(), i) &&
           !absl::c_linear_search(lhs->dimensions(), j) &&
           !absl::c_linear_search(rhs->dimensions(), i) &&
@@ -121,92 +129,128 @@ bool AlgebraicRewriterVisitor::MatchDistanceMatrix(
 }
 
 Status AlgebraicRewriterVisitor::HandleReduce(HloInstruction* reduce) {
-
   HloInstruction *x, *y;
+
   bool is_sub;
+
+  std::stringstream ss;
+
   int64_t x_dim, x_reduce_dim, x_dot_dim, y_dim, y_reduce_dim, y_dot_dim;
   if (!MatchDistanceMatrix(reduce, &x, &y, &is_sub, &x_dim, &x_reduce_dim,
-                           &y_dim, &y_reduce_dim))
+                           &y_dim, &y_reduce_dim)) {
     return Status::OK();
+  }
+
+  ss << ">>> ";
+  ss << "Rules match for the euclidean distance matrix. \n";
+  ss << "Distance X: " << x->name() << "\n";
+  ss << "Distance Y: " << y->name() << "\n";
+  ss << "X dimension: " << x_dim << "\n";
+  ss << "Y dimension: " << y_dim << "\n";
+  ss << "X reduce dimension: " << x_reduce_dim << "\n";
+  ss << "Y reduce dimension: " << y_reduce_dim << "\n";
+  ss << "Reduce operation: ";
+  ss << reduce->name() << "\n";
+
+  // HloComputation* reduce_sum = reduce->called_computations()[0];
+  HloComputation* comp = reduce->parent();
 
   // constants
-  HloInstruction* zero = reduce->parent()->AddInstruction(
+  HloInstruction* zero = comp->AddInstruction(
       HloInstruction::CreateConstant(LiteralUtil::CreateR0<float>(0)));
-  HloInstruction* two = reduce->parent()->AddInstruction(
+  HloInstruction* two = comp->AddInstruction(
       HloInstruction::CreateConstant(LiteralUtil::CreateR0<float>(2)));
+    
+  Shape x_shape = x->shape();
+  Shape y_shape = y->shape();
 
   // squares
-  HloInstruction* two_x = reduce->parent()->AddInstruction(
-      HloInstruction::CreateBroadcast(x->shape(), two, {}));
-  HloInstruction* x_squared = reduce->parent()->AddInstruction(
-      HloInstruction::CreateBinary(x->shape(), HloOpcode::kPower, x, two_x));
-  HloInstruction* two_y = reduce->parent()->AddInstruction(
-      HloInstruction::CreateBroadcast(y->shape(), two, {}));
-  HloInstruction* y_squared = reduce->parent()->AddInstruction(
-      HloInstruction::CreateBinary(y->shape(), HloOpcode::kPower, y, two_y));
+  HloInstruction* two_x = comp->AddInstruction(
+      HloInstruction::CreateBroadcast(x_shape, two, {}));
+  HloInstruction* x_squared = comp->AddInstruction(
+      HloInstruction::CreateBinary(x_shape, HloOpcode::kPower, x, two_x));
+  HloInstruction* two_y = comp->AddInstruction(
+      HloInstruction::CreateBroadcast(y_shape, two, {}));
+  HloInstruction* y_squared = comp->AddInstruction(
+      HloInstruction::CreateBinary(y_shape, HloOpcode::kPower, y, two_y));
 
   // reduce the squares
-  HloComputation* reduce_sum = reduce->called_computations()[0];
 
-  Shape x_reduce_shape = ShapeUtil::MakeShape(x_squared->shape().element_type(),
-                                              x_squared->shape().dimensions());
-  x_reduce_shape.DeleteDimension(x_reduce_dim);
-  HloInstruction* x_reduce =
-      reduce->parent()->AddInstruction(HloInstruction::CreateReduce(
-          x_reduce_shape, x_squared, zero, {x_reduce_dim}, reduce_sum));
+  Shape x_reduce_shape = x_squared->shape();
+  // x_reduce_shape.DeleteDimension(x_reduce_dim);
+  x_reduce_shape.set_dimensions(y_reduce_dim, 1);
+  HloInstruction* x_reduce = comp->AddInstruction(HloInstruction::CreateReduce(
+      x_reduce_shape, x_squared, zero, {x_reduce_dim}, comp));
 
-  Shape y_reduce_shape = ShapeUtil::MakeShape(y_squared->shape().element_type(),
-                                              y_squared->shape().dimensions());
-  y_reduce_shape.DeleteDimension(y_reduce_dim);
-  HloInstruction* y_reduce =
-      reduce->parent()->AddInstruction(HloInstruction::CreateReduce(
-          y_reduce_shape, y_squared, zero, {y_reduce_dim}, reduce_sum));
+  Shape y_reduce_shape = y_squared->shape();
+  // y_reduce_shape.DeleteDimension(y_reduce_dim);
+  y_reduce_shape.set_dimensions(y_reduce_dim, 1);
 
+  HloInstruction* y_reduce = comp->AddInstruction(HloInstruction::CreateReduce(
+      y_reduce_shape, y_squared, zero, {y_reduce_dim}, comp));
+
+  ss << "x_square_shape: " << x_squared->shape() << "\n";
+  ss << "y_square_shape: " << y_squared->shape() << "\n";
+  ss << "x_reduce_shape: " << x_reduce->shape() << "\n";
+  ss << "y_reduce_shape: " << y_reduce->shape() << "\n";
+  LOG(INFO) << ss.str();
+
+  // Start construction outer product
   // x y outer product
-  HloInstruction* xy;
-  if (x_dim == 0 && y_dim == 1) {
-    Shape xy_shape = ShapeUtil::MakeShape(x->shape().element_type(), {});
-    for (int64_t i = 0; i < x->shape().rank(); i++)
-      if (i != x_reduce_dim) xy_shape.add_dimensions(x->shape().dimensions(i));
-    for (int64_t i = 0; i < y->shape().rank(); i++)
-      if (i != y_reduce_dim) xy_shape.add_dimensions(y->shape().dimensions(i));
-
-    PrecisionConfig conf;
-    DotDimensionNumbers dnums;
-    dnums.add_lhs_contracting_dimensions(x_reduce_dim);
-    dnums.add_rhs_contracting_dimensions(y_reduce_dim);
-    xy = reduce->parent()->AddInstruction(
-        HloInstruction::CreateDot(xy_shape, x, y, dnums, conf));
-  } else if (x_dim == 1 && y_dim == 0) {
-    Shape xy_shape = ShapeUtil::MakeShape(x->shape().element_type(), {});
-    for (int64_t i = 0; i < y->shape().rank(); i++)
-      if (i != x_reduce_dim) xy_shape.add_dimensions(y->shape().dimensions(i));
-    for (int64_t i = 0; i < x->shape().rank(); i++)
-      if (i != y_reduce_dim) xy_shape.add_dimensions(x->shape().dimensions(i));
-
-    PrecisionConfig conf;
-    DotDimensionNumbers dnums;
-    dnums.add_lhs_contracting_dimensions(y_reduce_dim);
-    dnums.add_rhs_contracting_dimensions(x_reduce_dim);
-    xy = reduce->parent()->AddInstruction(
-        HloInstruction::CreateDot(xy_shape, y, x, dnums, conf));
+  auto lhs_reduce_dim = x_reduce_dim;
+  auto rhs_reduce_dim = y_reduce_dim;
+  HloInstruction* lhs = x;
+  HloInstruction* rhs = y;
+  if (x_dim < y_dim) {
+    lhs_reduce_dim = y_reduce_dim;
+    rhs_reduce_dim = x_reduce_dim;
+    lhs = y;
+    rhs = x;
   } else {
     // failed pre-condition :/
     CHECK((x_dim == 0 && y_dim == 1) || (x_dim == 1 && y_dim == 0));
   }
 
-  HloInstruction* x_broadcast = reduce->parent()->AddInstruction(
-      HloInstruction::CreateBroadcast(xy->shape(), x_reduce, {x_dim}));
-  HloInstruction* y_broadcast = reduce->parent()->AddInstruction(
-      HloInstruction::CreateBroadcast(xy->shape(), y_reduce, {y_dim}));
+  Shape outer_shape = lhs->shape();
+  Shape rhs_shape = rhs->shape();
+  outer_shape.DeleteDimension(lhs_reduce_dim);
+  rhs_shape.DeleteDimension(rhs_reduce_dim);
+  for (auto i = 0; i < rhs_shape.dimensions_size(); i++) {
+    outer_shape.add_dimensions(rhs_shape.dimensions(i));
+  }
 
-  HloInstruction* x_y_sum =
-      reduce->parent()->AddInstruction(HloInstruction::CreateBinary(
-          xy->shape(), HloOpcode::kAdd, x_broadcast, y_broadcast));
+  PrecisionConfig conf;
+  DotDimensionNumbers dnums;
+  dnums.add_lhs_contracting_dimensions(lhs_reduce_dim);
+  dnums.add_rhs_contracting_dimensions(rhs_reduce_dim);
+  HloInstruction* outer = comp->AddInstruction(
+      HloInstruction::CreateDot(outer_shape, lhs, rhs, dnums, conf));
+  
+  HloInstruction* x_broadcast = comp->AddInstruction(
+      HloInstruction::CreateBroadcast(outer_shape, x_reduce, {x_dim}));
+  HloInstruction* y_broadcast = comp->AddInstruction(
+      HloInstruction::CreateBroadcast(outer_shape, y_reduce, {y_dim}));
 
-  return ReplaceInstruction(
-      reduce, reduce->parent()->AddInstruction(HloInstruction::CreateBinary(
-                  xy->shape(), HloOpcode::kAdd, x_y_sum, xy)));
+  ss << "Outer product shape: " << outer->shape() << "\n";
+  ss << "x broadcast shape: " << x_broadcast->shape() << "\n";
+  ss << "y broadcast shape: " << y_broadcast->shape() << "\n";
+  LOG(INFO) << ss.str();
+
+  HloInstruction* x_y_sum = comp->AddInstruction(HloInstruction::CreateBinary(
+      outer_shape, HloOpcode::kAdd, x_broadcast, y_broadcast));
+
+  ss << "x_y_sum shape: " << x_y_sum->shape() << "\n";
+  LOG(INFO) << ss.str();
+
+  HloInstruction* replacement = comp->AddInstruction(
+      HloInstruction::CreateBinary(outer_shape, HloOpcode::kAdd, x_y_sum, outer));
+  
+  ss << "x_y_sum shape: " << x_y_sum->shape() << "\n";
+  ss << "replacement shape: " << replacement->shape() << "\n";
+  ss << "reduce shape: " << reduce->shape() << "\n";
+  LOG(INFO) << ss.str();
+
+  return ReplaceInstruction(reduce, replacement);
 }
 
 StatusOr<bool> AlgebraicRewriter::Run(HloModule* module) {
